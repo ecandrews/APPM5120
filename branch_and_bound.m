9d11981565524aa59f16ae7e1cdf773188ec3b3f
Elizabeth Andrews <liz@Elizabeths-MacBook-Air.local>
2026-04-09 19:58:22 -0600

    beginning branch-and-bound implementation, added obj fxn and precedence constraints

--- branch_and_bound.m
+++ branch_and_bound.m
@@ -7,31 +7,63 @@ Mixed-Integer Programming implementation using Branch-and-Bound to solve.
 clc;
 format long;
 
-% Load in the defined activities and constraints
-% Wait, actually I don't think I need to do this if I'm defining it below
+% Load in the defined activities, durations, and precedence relationships
 load('activity_defns.mat');
 
-% Define coefficients of objective function vector and integer variable
-% indices
-f = [1; 2; 3];
-intcon = 3;
+% At the end of the day, the "schedule" that we want out is a list of start
+% times for each of the activities. In order for the MIP solver to know
+% about these variables, we need to pass them in. But, our objective
+% function to minimize is only makespan (C). So we pass in zeroes for all
+% start times originally, and the solver will have the info it needs to
+% perform the optimization.
+% TODO: populate this in a loop or something so it can scale better?
+f = [activity_start_times(1, 1)... % Activity 1 start coefficient
+    activity_start_times(2,1)... % Activity 2 start coefficient
+    activity_start_times(3,1)... % Activity 3 start coefficient
+    activity_start_times(4,1)... % Activity 4 start coefficient
+    activity_start_times(5,1)... % Activity 5 start coefficient
+    1]; % makespan coefficient
+intcon = size(f);
 
-% Define inequality constraints (<=)
-Ain = [1, 1, 1];
-bin = 6;
+% Define inequality constraints. intlinprog expects constraints in the form
+% of "<=".
+% Constraints:
+%    C >= start_time + duration for all activities
+%         (becomes start_time - C <= -duration)
+%    start_j >= start_i + duration_i for precedence relationship where
+%    activity_i must occur before activity_j
+%         (becomes -start_j + start_i <= -duration_i
+% TODO: I'm just going to hardcode the precedence constraints for now just
+% to get it written down, and later I'll actually use the
+% precedence_constraints matrix
+A_inequal = [1, 0, 0, 0, 0, -1; % start_time1 - C
+             0, 1, 0, 0, 0, -1; % start_time2 - C
+             0, 0, 1, 0, 0, -1; % start_time3 - C
+             0, 0, 0, 1, 0, -1; % start_time4 - C
+             0, 0, 0, 0, 1, -1; % start_time5 - C
+             1, -1, 0, 0, 0, 0; % -start_time2 + start_time1
+             0, 1, 0, -1, 0, 0; % -start_time4 + start_time2
+             0, 0, 1, -1, 0, 0; % -start_time4 + start_time3
+             -1, 0, 0, 0, 1, 0]; % -start_time1 + start_time5
+b_inequal = [-1 * activity_durations(1,1); % -duration1
+             -1 * activity_durations(2,1); % -duration2
+             -1 * activity_durations(3,1); % -duration3
+             -1 * activity_durations(4,1); % -duration4
+             -1 * activity_durations(5,1); % -duration5
+             -1 * activity_durations(1,1); % -duration1
+             -1 * activity_durations(2,1); % -duration2
+             -1 * activity_durations(3,1); % -duration3
+             -1 * activity_durations(5,1)]; % -duration5
 
-% Define equality constraints
-Aeq = [1, 0, 0];
-beq = 2;
-
-% Define lower and upper bounds. Lower bounds are 0 for all variables and
-% upper bounds are x1, x2 >= 0 and x3 is a binary variable.
-lb = zeros(3, 1);
-ub = [Inf; Inf; 1];
+% Define lower and upper bounds for solution vector. All start times and
+% the makespan must be greater than or equal to zero, so lowerbound is
+% initialized to all zeroes. No upperbound.
+lb = zeros(6, 1);
+ub = [Inf; Inf; Inf; Inf; Inf; Inf];
 
 % Call solver function
 options = optimoptions('intlinprog','Display','iter','PlotFcn','optimplotmilp');
-[soln_vec, obj_val, exitflag, output] = intlinprog(f, intcon, Ain, bin, Aeq, beq, lb, ub);
+[soln_vec, obj_val, exitflag, output] = intlinprog(f, intcon, A_inequal, b_inequal, [], [], lb, ub);
 
 % Display results in some sort of graphical format to use in report?
 % Shouldn't the plotfcn do that for me?
--- create_act_defns.m
+++ create_act_defns.m
@@ -5,12 +5,41 @@ mixed-integer programming brach-and-bound implementation, and the iterative
 conflict repair implementation. Faster to create and import a .mat file
 than to hard code this into both scripts separately.
 
+Activity 1:
+    Duration - 1 hour
+    Must occur before Activity 2
+Activity 2:
+    Duration - 3 hours
+    Must occur before Activity 4
+Activity 3:
+    Duration - 5 hours
+    Must occur before Activity 4
+Activity 4: 
+    Duration - 2 hours
+    No precedence constraint
+Activity 5:
+    Duration - 6 hours
+    Must occur before Activity 1
+
 %}
 
 clc;
 
-test_x=3;
-test_y=4;
-test_z="hello world!";
+% Define activity durations in units of hours
+activity_durations = [1; 3; 5; 2; 6];
 
-save('activity_defns.mat')
+% Define precedence constraints (P⊆{(i, j) | i,j∈T, i must occur before j})
+% Matrix is mxn
+% For index_mn:
+%    - 0: Activity m has no constraint with Activity n
+%    - 1: Activity m must complete before Activity n starts
+precedence_constraints = [0, 1, 0, 0, 0;
+                          0, 0, 0, 1, 0;
+                          0, 0, 0, 1, 0;
+                          0, 0, 0, 0, 0;
+                          1, 0, 0, 0, 0];
+
+% Define initial start times for activities
+activity_start_times = [0; 0; 0; 0; 0];
+
+save('activity_defns.mat');
